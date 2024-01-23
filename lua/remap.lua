d56906ef392b12f7b5b56fa51b36acb1a01c7cb4
Estefan Hu <estefamh@amazon.com>
2024-01-23 09:53:04 -0800

    Easy pane navigation

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', "<c-h>", "<C-w>h")
+vim.keymap.set('n', "<c-j>", "<C-w>j")
+vim.keymap.set('n', "<c-k>", "<C-w>k")
+vim.keymap.set('n', "<c-l>", "<C-w>l")
